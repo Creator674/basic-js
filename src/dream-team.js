62a5ca5b7dfebc4b2ebb18c2174c28d389101631
YaroslavR <666anastyalena@mail.ru>
2020-03-10 23:32:11 +0300

    feat: complete DreamTeam task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)){
+    return false;
+  }
+let answer = [];
+members.sort();
+for(let i = 0; i < members.length; i++){
+  if(typeof members[i] == 'string'){
+    members[i] = members[i].replace(/\s+/g,'');
+    answer.push(members[i][0]);
+  }
+}
+for(let i = 0; i < answer.length; i++){
+  answer[i] = answer[i].toUpperCase();
+}
+
+return answer.sort().join('');
 };
